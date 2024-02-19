374f7d9c5d74e5227b17db2fb72542036daf58ba
Luiz Felipe Fornari <luizfelipefornari@GMAIL.COM>
2024-02-19 14:53:22 -0300

    fixing carregando.jsx

--- src/componentes/comuns/Carregando.jsx
+++ src/componentes/comuns/Carregando.jsx
@@ -1,17 +1,17 @@
 import { Center, CircularProgress } from "@chakra-ui/react";
 
 function Carregando(props) {
-    return (
-      <>
-        {
-          !props.carregando ?  props.children  :
-          <Center >
-            <CircularProgress isIndeterminate />
-          </Center>
-        }
-      </>
-  
-    )
-  }
-  
+  return (
+    <>
+      {!props.carregando ? (
+        props.children
+      ) : (
+        <Center h="500px">
+          <CircularProgress isIndeterminate />
+        </Center>
+      )}
+    </>
+  );
+}
+
 export default Carregando;
